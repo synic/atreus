38dacdc9f54a02ee5709f922ae7fa5c055d648fc
Adam Olsen <arolsen@gmail.com>
2020-10-17 10:40:16 -0600

    lign up keymap columns

--- vimatreus.ino
+++ vimatreus.ino
@@ -61,41 +61,41 @@ enum {
 KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
   (
-       Key_Q            ,Key_W   ,Key_E       ,Key_R         ,Key_T
-      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+       Key_Q                    ,Key_W                    ,Key_E                    ,Key_R                    ,Key_T
+      ,Key_A                    ,Key_S                    ,Key_D                    ,Key_F                    ,Key_G
+      ,Key_Z                    ,Key_X                    ,Key_C                    ,Key_V                    ,Key_B                    ,Key_Backtick
+      ,Key_Esc                  ,Key_Tab                  ,Key_LeftGui              ,Key_LeftShift            ,Key_Backspace            ,Key_LeftControl
 
-                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
-                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
-       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,TD(TD_ALT)   ,Key_Space ,TD(TD_FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+                                ,Key_Y                    ,Key_U                    ,Key_I                    ,Key_O                    ,Key_P
+                                ,Key_H                    ,Key_J                    ,Key_K                    ,Key_L                    ,Key_Semicolon
+      ,Key_Backslash            ,Key_N                    ,Key_M                    ,Key_Comma                ,Key_Period               ,Key_Slash
+      ,TD(TD_ALT)               ,Key_Space                ,TD(TD_FUN)               ,Key_Minus                ,Key_Quote                ,Key_Enter
   ),
 
   [FUN] = KEYMAP_STACKED
   (
-       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
-      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
-      ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
-      ,LockLayer(UPPER),Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete            ,Key_LeftControl
+       Key_Exclamation          ,Key_At                   ,Key_UpArrow              ,Key_Dollar               ,Key_Percent
+      ,Key_LeftParen            ,Key_LeftArrow            ,Key_DownArrow            ,Key_RightArrow           ,Key_RightParen
+      ,Key_LeftBracket          ,Key_RightBracket         ,Key_Hash                 ,Key_LeftCurlyBracket     ,Key_RightCurlyBracket    ,Key_Caret
+      ,LockLayer(UPPER)         ,Key_Insert               ,Key_LeftGui              ,Key_LeftShift            ,Key_Delete               ,Key_LeftControl
 
-                       ,Key_PageUp       ,Key_7 ,Key_8      ,Key_9 ,Key_Home
-                       ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,Key_End
-      ,Key_And         ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
-      ,Key_LeftAlt     ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
+                                ,Key_PageUp               ,Key_7                    ,Key_8                    ,Key_9                    ,Key_Home
+                                ,Key_PageDown             ,Key_4                    ,Key_5                    ,Key_6                    ,Key_End
+      ,Key_And                  ,Key_Star                 ,Key_1                    ,Key_2                    ,Key_3                    ,Key_Plus
+      ,Key_LeftAlt              ,Key_Space                ,___                      ,Key_Period               ,Key_0                    ,Key_Equals
    ),
 
   [UPPER] = KEYMAP_STACKED
   (
-       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
-      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
-      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
-      ,MoveToLayer(QWERTY)   ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+       Key_Insert               ,Key_Home                 ,Key_UpArrow              ,Key_End                  ,Key_PageUp
+      ,Key_Delete               ,Key_LeftArrow            ,Key_DownArrow            ,Key_RightArrow           ,Key_PageDown
+      ,M(MACRO_VERSION_INFO)    ,Consumer_VolumeIncrement ,XXX                      ,XXX                      ,___                      ,___
+      ,MoveToLayer(QWERTY)      ,Consumer_VolumeDecrement ,___                      ,___                      ,___                      ,___
 
-                             ,Key_UpArrow   ,Key_F7              ,Key_F8          ,Key_F9         ,Key_F10
-                ,Key_DownArrow ,Key_F4              ,Key_F5          ,Key_F6         ,Key_F11
-      ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
-      ,___      ,___           ,MoveToLayer(QWERTY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
+                                ,Key_UpArrow              ,Key_F7                   ,Key_F8                   ,Key_F9                   ,Key_F10
+                                ,Key_DownArrow            ,Key_F4                   ,Key_F5                   ,Key_F6                   ,Key_F11
+      ,___                      ,XXX                      ,Key_F1                   ,Key_F2                   ,Key_F3                   ,Key_F12
+      ,___                      ,___                      ,MoveToLayer(QWERTY)      ,Key_PrintScreen          ,Key_ScrollLock           ,Consumer_PlaySlashPause
    )
 )
 /* *INDENT-ON* */
@@ -115,9 +115,9 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
     }
     break;
   case MACRO_VIM_ESCAPE:
-    return MACRODOWN(I(25), T(Esc), I(25), T(Esc));
+    return MACRODOWN(I(30), T(Esc), I(25), T(Esc));
   case MACRO_VIM_SAVE:
-    return MACRODOWN(I(25), T(Esc), I(25), T(Esc), T(Colon), T(W), T(Enter));
+    return MACRODOWN(I(30), T(Esc), I(50), T(Esc), T(Colon), T(W), T(Enter));
   case MACRO_UNIX_TILDE:
     return MACRODOWN(I(25), T(Tilde), T(Slash));
   default:
